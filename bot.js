2c49b4868874b2f61a36b751632c8054bbab093b
karpovegor235 <karpovegor235@gmail.com>
2026-04-30 15:19:41 +0300

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,10 @@
+const TelegramBot = require('node-telegram-bot-api');
+
+const TOKEN = '8376903942:AAHm2T5QaAFQbrhu_ivhf7-11-ZH9S-D_zQ';
+const bot = new TelegramBot(TOKEN, { polling: true });
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, '✨ Бот работает! Напиши /help');
+});
+
+console.log('Бот запущен');
